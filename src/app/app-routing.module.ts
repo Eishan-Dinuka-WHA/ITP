0d5b76ce89a06e0246f491078637af0632532245
EishanDinuka <eishan2dinuka@gmail.com>
2020-09-06 13:44:19 +0530

    header-footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,14 @@ import { HeaderComponent } from './header/header.component';
 import { CustomerViewComponent } from './customer-registration/customer-view/customer-view.component';
 import { CustomerNewComponent } from './customer-registration/customer-new/customer-new.component';
 import { NavibarComponent } from './navibar/navibar.component';
+import { HomeCardComponent } from './home-page/home-card/home-card.component';
 
 const appRoutes: Routes =[
   { path: '' , component: HomePageComponent },
   { path: 'cus-new' , component:CustomerNewComponent} ,
   { path: 'header' , component:HeaderComponent },
   { path: 'navibar' , component:NavibarComponent },
-  { path: 'navibar' , component:NavibarComponent },
+  { path: 'homecard' , component:HomeCardComponent },
   { path: 'navibar' , component:NavibarComponent },
   { path: 'navibar' , component:NavibarComponent },
   { path: 'navibar' , component:NavibarComponent },
